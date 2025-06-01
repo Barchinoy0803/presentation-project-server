a13f40673335d3ed0e53ccb586b1bc9df30f9f0d
Barchinoy0803 <barchinoyyusupova33@gmail.com>
2025-06-01 22:11:33 +0500

    some features added

--- src/presentation/presentation.service.ts
+++ src/presentation/presentation.service.ts
@@ -1,17 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreatePresentationDto } from './dto/create-presentation.dto';
 import { UpdatePresentationDto } from './dto/update-presentation.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class PresentationService {
+  constructor(
+    private readonly prisma: PrismaService
+  ) { }
+
   create(createPresentationDto: CreatePresentationDto) {
     return 'This action adds a new presentation';
   }
 
-  findAll() {
-    return `This action returns all presentation`;
-  }
 
+  async findAll() {
+    try {
+      let presentations = await this.prisma.presentation.findMany({
+        include: {
+          slides: true,
+          users: true
+        }
+      })
+      if (!presentations) throw new NotFoundException("Not found presentation!")
+      return presentations
+    } catch (error) {
+      console.log(error);
+    }
+  }
   findOne(id: number) {
     return `This action returns a #${id} presentation`;
   }
--- src/socket/socket.service.ts
+++ src/socket/socket.service.ts
@@ -1,15 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateSocketDto } from './dto/create-socket.dto';
 import { UpdateSocketDto } from './dto/update-socket.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class SocketService {
+  constructor(
+    private readonly prisma: PrismaService
+  ) { }
+
   create(createSocketDto: CreateSocketDto) {
     return 'This action adds a new socket';
   }
 
-  findAll() {
-    return `This action returns all socket`;
+  async findAll() {
+    try {
+      let presentations = await this.prisma.presentation.findMany()
+      if (!presentations) throw new NotFoundException("Not found presentation!")
+      return presentations
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   findOne(id: number) {
